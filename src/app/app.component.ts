2b410a2b6b235a0d115447da28858558d53a49a3
Füzi Botond <fuziboti@gmail.com>
2024-12-09 14:22:29 +0100

    Frontend page for listing panic attacks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,75 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Router, RouterOutlet } from '@angular/router';
+import { MatTabsModule} from '@angular/material/tabs';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component'
+import { PanicAttacksComponent } from './components/panic-attacks/panic-attacks.component';
+import { SleepTrackerComponent } from './components/sleep-tracker/sleep-tracker.component';
+import { SleepQualityComponent } from './components/sleep-quality/sleep-quality.component';
+import { HeartRateComponent } from './components/heart-rate/heart-rate.component';
+import { AlertHistoryComponent } from './components/alert-history/alert-history.component';
+import { ActivatedRoute } from '@angular/router';
+import { NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule, RouterOutlet, RouterLink, RouterLinkActive,
+    MatTabsModule,
+    MatToolbarModule,
+    ProfileComponent,
+    PanicAttacksComponent,
+    SleepTrackerComponent,
+    SleepQualityComponent,
+    HeartRateComponent,
+    AlertHistoryComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  activeLinkIndex = -1;
+  constructor(private route: ActivatedRoute, public router: Router){}
+  navigation: any = [
+    {
+      label: 'Profile',
+      link: '/profile',
+      index: 0
+    },
+    {
+      label: 'Panic attacks',
+      link: '/panic-attacks',
+      index: 1
+    },
+    {
+      label: 'Sleep quality',
+      link: '/sleep-quality',
+      index: 2
+    },
+    {
+      label: 'Sleep tracker',
+      link: '/sleep-tracker',
+      index: 3
+    },
+    {
+      label: 'Heart rate',
+      link: '/heart-rate',
+      index: 4
+    }
+  ];
   title = 'CalmWatchFrontend';
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        console.log('Tab changed to:', event.url);
+        // Optionally perform actions based on the URL
+      });
+  }
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PanicAttacksComponent } from './components/panic-attacks/panic-attacks.component';
+import { SleepTrackerComponent } from './components/sleep-tracker/sleep-tracker.component';
+import { SleepQualityComponent } from './components/sleep-quality/sleep-quality.component';
+import { HeartRateComponent } from './components/heart-rate/heart-rate.component';
+import { AlertHistoryComponent } from './components/alert-history/alert-history.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'panic-attacks', component: PanicAttacksComponent },
+  { path: 'sleep-tracker', component: SleepTrackerComponent },
+  { path: 'sleep-quality', component: SleepQualityComponent },
+  { path: 'heart-rate', component: HeartRateComponent },
+  { path: 'alert-history', component: AlertHistoryComponent },
+];
+
+
+
